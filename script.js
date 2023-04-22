958a0c99c79ac821bfdbe65238b44efcd7b9e906
Vitus Ayariga <vitusayariga@gmail.com>
2023-04-22 16:37:39 +0000

    working on social media share

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+////////////////////////////////////////////////////////////
+// HTML ELEMENTS
 // selecting html elements
 const quoteEl = document.querySelector(".quote");
 const authorEl = document.querySelector(".author");
@@ -10,35 +12,19 @@ const socialIcons = document.querySelector(".socialIcons");
 const shareBtn = document.querySelector(".share");
 const newBtn = document.querySelector(".new");
 const htmlSection = document.querySelector("section");
-const loaderEl = document.querySelector('.loader')
-const htmlMain = document.querySelector('main');
+const loaderEl = document.querySelector(".loader");
+const htmlMain = document.querySelector("main");
 
+// socialIcons
+const twitterBtn = document.querySelector(".twitter");
+const facebookBtn = document.querySelector(".facebook");
+const instagramBtn = document.querySelector(".instagram");
+const whatsappBtn = document.querySelector(".whatsapp");
 
+////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES
 // set quotes as a global variable
 let quotes = [];
-
-
-// function to display loader 
-function showLoader() { 
-  loaderEl.style.display = "block";
-  htmlMain.style.display = "none";
-}
-
-// function to hide loader
-function hideLoader() { 
-  loaderEl.style.display = "none";
-  htmlMain.style.display ='flex';
-}
-
-// this function updates the content of the html elements (quote & author)
-function newQuote() {
-  showLoader();
-
-  quoteEl.textContent = quotes[0].quote;
-  authorEl.textContent = quotes[0].author;
-  quoteContainer.classList.add("show");
-  authorContainer.classList.add("show");
-}
 
 // API options object
 const apiConfig = {
@@ -48,7 +34,44 @@ const apiConfig = {
   },
 };
 
-// Get data from API using async function
+// displayed content
+const displayedQuote = quoteEl.textContent;
+const displayedAuthor = authorEl.textContent;
+
+// social media urls
+const url = encodeURIComponent(window.location.href);
+const text = encodeURIComponent(`${displayedQuote} - ${displayedAuthor}`);
+const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}&quote=${text}`;
+const instagramUrl = `https://www.instagram.com/share?url=${url}&caption=${text}`;
+const whatsappUrl = `https://api.whatsapp.com/send?text=${text}`;
+const twitterUrl = `https://twitter.com/intent/tweet?url=${url}&text=${text}&via=yourusername`;
+
+
+////////////////////////////////////////////////////////////
+// FUNCTIONS
+// function to display loader
+function showLoader() {
+  loaderEl.style.display = "block";
+  htmlMain.style.display = "none";
+}
+
+// function to hide loader
+function hideLoader() {
+  loaderEl.style.display = "none";
+  htmlMain.style.display = "flex";
+}
+
+// function to update content of html elements (quote & author)
+function newQuote() {
+  showLoader();
+
+  quoteEl.textContent = quotes[0].quote;
+  authorEl.textContent = quotes[0].author;
+  quoteContainer.classList.add("show");
+  authorContainer.classList.add("show");
+}
+
+// async function to get data from API using
 const fetchQuotes = async function () {
   try {
     // initial loading
@@ -62,7 +85,7 @@ const fetchQuotes = async function () {
     // converting data to JSON
     quotes = await response.json();
     newQuote();
-    // removing loader 
+    // removing loader
     hideLoader();
   } catch (err) {
     // errors are catched here
@@ -85,15 +108,29 @@ function closeSocialIcons() {
   socialIcons.style.display = "none";
 }
 
+////// functions for sharing content
+// function for twitter
+function shareToSocialMedia(url) {
+  window.open(url, "_blank");
+}
+
 // onload
 showLoader();
 fetchQuotes();
 
-// clicking the New button
+////////////////////////////////////////////////////////////
+// CLICKS
+// the New button
 newBtn.addEventListener("click", fetchQuotes);
 
-// clicking the Shared button
+// the Shared button
 shareBtn.addEventListener("click", newShare);
 
-// clicking outside of social icons card(blurEL) should close the popup
+// outside of social icons card(blurEL) should close the popup
 blurEl.addEventListener("click", closeSocialIcons);
+
+///// social icons
+twitterBtn.addEventListener("click", () => shareToSocialMedia(twitterUrl));
+facebookBtn.addEventListener("click", () => shareToSocialMedia(facebookUrl));
+whatsappBtn.addEventListener("click", () => shareToSocialMedia(whatsappUrl));
+instagramBtn.addEventListener("click", () => shareToSocialMedia(instagramUrl));
